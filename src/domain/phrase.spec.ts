ec22a50b4c3dc76f9e2bc4e08ab214cdb847325f
Filipe <filipe.cdesouza@gmail.com>
2021-09-06 18:42:51 -0300

    test: implement method isphraseOnlyOneChangePerDay

--- src/domain/phrase.spec.ts
+++ src/domain/phrase.spec.ts
@@ -8,16 +8,20 @@ const pharse = 'any phrase';
 
 const makeSut = (): SutTypes => {
   const sut = new Phrase(pharse);
-
   return {
     sut
   };
 };
 
 describe('phrase', () => {
-  test('should a receive a new phrase with 2 words', () => {
+  test('Should a receive a new phrase with 2 words', () => {
     const { sut } = makeSut();
     const isValidPhrase = sut.isphraseHaveMinimumTwoWords();
     expect(isValidPhrase).toBe(true);
+  });
+  test('Should ensure that the phrase hasn t changed today', () => {
+    const { sut } = makeSut();
+    const onlyOneChangePhraseEveryDay = sut.isphraseOnlyOneChangePerDay();
+    expect(onlyOneChangePhraseEveryDay).toBe(true);
   });
 });
