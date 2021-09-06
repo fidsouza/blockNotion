904aa15aeabafffb8512921e9c6048ff4cc412f3
Filipe <filipe.cdesouza@gmail.com>
2021-09-06 20:26:12 -0300

    test: implement formatDatePtBR

---
+++ src/utils/formatDate.spec.ts
@@ -0,0 +1,17 @@
+import { FormatDate } from './formatDate';
+
+const makeDate = () => {
+  const date = new Date(2014, 1, 11);
+
+  const formatDateinPtBR = new FormatDate(date).formatDatePtBR();
+  return {
+    formatDateinPtBR
+  };
+};
+
+describe('Parse date', () => {
+  test('should return a parse date in format Pt-BR', () => {
+    const { formatDateinPtBR } = makeDate();
+    expect(formatDateinPtBR).toEqual('11/02/2014');
+  });
+});
