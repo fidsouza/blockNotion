14a360d0a0751ab304989ec9356582f5d913e720
Filipe <filipe.cdesouza@gmail.com>
2021-09-07 21:01:58 -0300

    test: implement method is phrase only one change per day

--- src/domain/phrase.spec.ts
+++ src/domain/phrase.spec.ts
@@ -1,3 +1,7 @@
+import { writeFileSync } from 'fs';
+
+import { formatType } from '../utils/formatDates/ports/formatType';
+
 import { Phrase } from './phrase';
 
 interface SutTypes {
@@ -6,14 +10,28 @@ interface SutTypes {
 
 const pharse = 'any phrase';
 
+const makeFormatDate = (): formatType => {
+  class FormatTypeDate implements formatType {
+    formatDatePtBR(): string {
+      const dateNow = '01/09/2021';
+      return dateNow;
+    }
+  }
+  return new FormatTypeDate();
+};
+
 const makeSut = (): SutTypes => {
-  const sut = new Phrase(pharse);
+  const sut = new Phrase(pharse, makeFormatDate());
   return {
     sut
   };
 };
 
 describe('phrase', () => {
+  beforeAll(async () => {
+    const contentFile = { lastUpdate: '06/09/2021' };
+    writeFileSync('lastUpdatePhrase-test.json', JSON.stringify(contentFile));
+  });
   test('Should a receive a new phrase with 2 words', () => {
     const { sut } = makeSut();
     const isValidPhrase = sut.isphraseHaveMinimumTwoWords();
